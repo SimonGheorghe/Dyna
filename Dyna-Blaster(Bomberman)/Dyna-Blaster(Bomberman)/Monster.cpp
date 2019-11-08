27bd910485c9702fb385f0e5b078b902289476d3
Simon.Gheorghe <zymygheorghe@gmail.com>
2019-11-08 18:29:02 +0200

    Added Monster class. Contains class members, constructor, getters, list of non-Boss monsters.

---
+++ Dyna-Blaster(Bomberman)/Dyna-Blaster(Bomberman)/Monster.cpp
@@ -0,0 +1,178 @@
+#include "Monster.h"
+
+Monster::Monster(Type type) :
+	m_type(type)
+{
+	switch (type)
+	{
+	case Monster::Type::Ballom:
+	{
+		m_speed = Speed::Slow;
+		m_hitPoints = 1;
+		m_score = 100;
+		m_passingAbility = false;
+	}
+		break;
+	case Monster::Type::Ekutopu:
+	{
+		m_speed = Speed::Slow;
+		m_hitPoints = 1;
+		m_score = 200;
+		m_passingAbility = false;
+	}
+		break;
+	case Monster::Type::Boyon:
+	{
+		m_speed = Speed::Slow;
+		m_hitPoints = 1;
+		m_score = 200;
+		m_passingAbility = false;
+	}
+		break;
+	case Monster::Type::Pass:
+	{
+		m_speed = Speed::VeryFast;
+		m_hitPoints = 1;
+		m_score = 500;
+		m_passingAbility = false;
+	}
+		break;
+	case Monster::Type::Telpio:
+	{
+		m_speed = Speed::Fast;
+		m_hitPoints = 1;
+		m_score = 400;
+		m_passingAbility = true;
+	}
+		break;
+	case Monster::Type::Pomori:
+	{
+		m_speed = Speed::VeryFast;
+		m_hitPoints = 1;
+		m_score = 600;
+		m_passingAbility = true;
+	}
+		break;
+	case Monster::Type::Onil:
+	{
+		m_speed = Speed::Slow;
+		m_hitPoints = 1;
+		m_score = 200;
+		m_passingAbility = false;
+	}
+		break;
+	case Monster::Type::Gacha:
+	{
+		m_speed = Speed::Normal;
+		m_hitPoints = 1;
+		m_score = 300;
+		m_passingAbility = false;
+	}
+		break;
+	case Monster::Type::Minvo:
+	{
+		m_speed = Speed::Slow;//?
+		m_hitPoints = 1;
+		m_score = 700;
+		m_passingAbility = false;
+	}
+		break;
+	case Monster::Type::Buffer:
+	{
+		m_speed = Speed::Slow;
+		m_hitPoints = 1;
+		m_score = 300;
+		m_passingAbility = false;
+	}
+		break;
+	case Monster::Type::Peggi:
+	{
+		m_speed = Speed::Slow;
+		m_hitPoints = 1;
+		m_score = 200;
+		m_passingAbility = false;
+	}
+		break;
+	case Monster::Type::Flapper:
+	{
+		m_speed = Speed::Fast;
+		m_hitPoints = 1;
+		m_score = 400;
+		m_passingAbility = false;
+	}
+		break;
+	case Monster::Type::BakeBake:
+	{
+		m_speed = Speed::Normal;//?
+		m_hitPoints = 1;
+		m_score = 200;
+		m_passingAbility = false;
+	}
+		break;
+	case Monster::Type::Korisuke:
+	{
+		m_speed = Speed::Slow;//?
+		m_hitPoints = 1;
+		m_score = 200;
+		m_passingAbility = false;
+	}
+		break;
+	case Monster::Type::Maron:
+	{
+		m_speed = Speed::Normal;
+		m_hitPoints = 1;
+		m_score = 300;
+		m_passingAbility = false;
+	}
+		break;
+	case Monster::Type::Ojin://?
+	{
+		m_speed = Speed::Normal;
+		m_hitPoints = 1;
+		m_score = 400;
+		m_passingAbility = false;
+	}
+		break;
+	case Monster::Type::Pontan:
+	{
+		m_speed = Speed::VeryFast;
+		m_hitPoints = 1;
+		m_score = 1000;
+		m_passingAbility = false;
+	}
+		break;
+	default:
+	{
+		m_speed = Speed::None;
+		m_hitPoints = 0;
+		m_score = 0;
+		m_passingAbility = false;
+	}
+		break;
+	}
+}
+
+Monster::Type Monster::GetType()
+{
+	return m_type;
+}
+
+Monster::Speed Monster::GetSpeed()
+{
+	return m_speed;
+}
+
+uint16_t Monster::GetHitPoints()
+{
+	return m_hitPoints;
+}
+
+uint16_t Monster::GetScore()
+{
+	return m_score;
+}
+
+bool Monster::GetPassingAbility()
+{
+	return m_passingAbility;
+}
---
+++ Dyna-Blaster(Bomberman)/Dyna-Blaster(Bomberman)/Monster.h
@@ -0,0 +1,51 @@
+#pragma once
+#include<iostream>
+#include<cstdint>
+class Monster
+{
+public:
+	enum class Type
+	{
+		Ballom,
+		Ekutopu,
+		Boyon,
+		Pass,
+		Telpio,
+		Pomori,
+		Onil,
+		Gacha,
+		Minvo,
+		Buffer,
+		Peggi,
+		Flapper,
+		BakeBake,
+		Korisuke,
+		Maron,
+		Ojin,
+		Pontan
+	};
+	enum class Speed
+	{
+		None = 0,
+		Slow,
+		Normal,
+		Fast,
+		VeryFast
+	};
+
+public:
+	Monster(Type type);
+	Type GetType();
+	Speed GetSpeed();
+	uint16_t GetHitPoints();
+	uint16_t GetScore();
+	bool GetPassingAbility();
+
+private:
+	Type m_type;
+	Speed m_speed;
+	uint16_t m_hitPoints;
+	uint16_t m_score;
+	bool m_passingAbility;
+};
+

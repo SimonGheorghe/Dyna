070017e90725f50f224e61dd6715b46fb57b656f
louiscomnoiu <louis.comnoiu@gmail.com>
2019-11-09 22:52:28 +0200

    Optimized Bomb class.

--- Dyna-Blaster(Bomberman)/Dyna-Blaster(Bomberman)/Bomb.cpp
+++ Dyna-Blaster(Bomberman)/Dyna-Blaster(Bomberman)/Bomb.cpp
@@ -1,6 +1,70 @@
 #include "Bomb.h"
 
-Bomb::Bomb()
+Bomb::Bomb(int X, int Y)
 {
-	//empty
+	//this is a blastlet
+	SetBlast(true);
+
+}
+
+//destructor is important for destroying old bomb objects after they have detonated!
+Bomb::~Bomb() {
+
+	delete this;
+}
+
+void Bomb::SetUsed(bool u) {
+
+	IsUsed = u;
+}
+
+bool Bomb::GetBlast() {
+
+	return IsBlast;
+}
+
+void Bomb::SetBlast(bool i) {
+
+	IsBlast = i;
+}
+
+bool Bomb::GetIgnite() {
+
+	return Ignition;
+}
+
+void Bomb::SetIgnite(bool i) {
+
+	Ignition = i;
+}
+
+bool Bomb::GetUsed() {
+
+	return IsUsed;
+}
+
+
+uint16_t Bomb::GetBombTicks() {
+
+	return m_bombTicks;
+}
+
+//returns length of blast
+uint16_t Bomb::GetFlame() {
+
+	return m_flame;
+}
+
+//sets timer on newbombs
+void Bomb::SetBombTicks(uint16_t T)
+{
+
+	m_bombTicks = T;
+}
+
+//sets flame size on new bombs
+void Bomb::SetFlame(uint16_t F) 
+{
+
+	m_flame = F;
 }
--- Dyna-Blaster(Bomberman)/Dyna-Blaster(Bomberman)/Bomb.h
+++ Dyna-Blaster(Bomberman)/Dyna-Blaster(Bomberman)/Bomb.h
@@ -1,39 +1,50 @@
 #pragma once
 #include <iostream>
-
+#include <cstdint>
 
 class Bomb
 {
 public:
+	//this constructor is for blast, these are for the blast that travels  up down left and right of where it had detonated
+	  //these are to be created and destroyed and to kill the player on contact
+	Bomb(int, int);
 
-private:
-	size_t tick;
-	size_t id;
-	size_t posX;
-	size_t posY;
-	size_t pwr;
-	size_t blow;
-
-public:
-	Bomb(size_t, size_t, size_t, size_t);
-	Bomb();
 	~Bomb();
 
-public:
-	void draw(void);
+	//getters for returning ticks time and flame size
+	uint16_t GetBombTicks();
+	uint16_t GetFlame();
 
-	void onFire(void);
+	//sets timer on newbombs
+	void SetBombTicks(uint16_t _T);
 
-public:
-	bool blewUp(void) const;
-	size_t getX(void) const;
-	size_t getY(void) const;
-	size_t getPower(void) const;
-	size_t getId(void) const;
-	size_t getTicks(void) const;
+	//sets flame size on new bombs
+	void SetFlame(uint16_t _F);
 
-public:
-	void serialize(std::ostream& stream) const;
+
+
+	//sets the sprites position when it is created
+	void SetMonsterPos();
+
+	//Setter and getter for bomb if it has been used
+	void SetUsed(bool u);
+	bool GetUsed();
+
+	//Setter and getter for bomb ignition flag
+	void SetIgnite(bool i);
+	bool GetIgnite();
+
+	//Get if blast or not
+	bool GetBlast();
+	void SetBlast(bool i);
+
+	private:
+
+		//Ticks is the original bombs timer flame is for how many blastlets to create after bomb has detonated
+		uint16_t m_bombTicks, m_flame;
+
+		//This is for if the bomb is a bomb or a blast second is if the bombs living time
+		//Is over and has to be removed from the array after it has gone off, ignition is the
+		//State
+		bool IsBlast, IsUsed, Ignition;
 };
-
-

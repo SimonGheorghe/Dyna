502b8d128aa8772ab8c0d765c8fe7267fc1cf962
LucianStanila <lucianstanila02@yahoo.com>
2019-11-24 23:04:40 +0200

    Added Logger Dynamic-link library.

--- Dyna-Blaster(Bomberman)/Dyna-Blaster(Bomberman)/main.cpp
+++ Dyna-Blaster(Bomberman)/Dyna-Blaster(Bomberman)/main.cpp
@@ -1,7 +1,11 @@
 #include "DynaGame.h"
-
+#include"../Logging/Logger.h"
+#include<fstream>
 void main()
 {
+	std::ofstream logFile("log.log", std::ios::app);
+	Logger logger(logFile, Logger::Level::Info);
+	logger.Log("Start aplication.", Logger::Level::Info);
 	DynaGame game;
 	game.Run();
 }

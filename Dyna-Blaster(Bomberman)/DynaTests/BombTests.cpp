5b5f3310e1047e72d89c709d47960eaad47e96cd
Ionatan <binzaruionatan@yahoo.com>
2019-12-07 16:07:49 +0200

    Added the last test for bomb class. For display.

--- Dyna-Blaster(Bomberman)/DynaTests/BombTests.cpp
+++ Dyna-Blaster(Bomberman)/DynaTests/BombTests.cpp
@@ -12,9 +12,17 @@ namespace DynaTests
 		TEST_METHOD(Constructor)
 		{
 			uint16_t coordX = 3, coordY = 2, id = 2;
-			Bomb bomm(coordX, coordY, id);
-			Assert::IsTrue(bomm.GetCoordX == 3);
+			Bomb bomb(coordX, coordY, id);
+			Assert::IsTrue(bomb.GetCoordX == 3);
 		}
 
+		TEST_METHOD(Print)
+		{
+			uint16_t coordX = 0, coordY = 0, id = 0;
+			Bomb bomb(coordX, coordY, id);
+			std::stringstream stream;
+			stream << bomb;
+			Assert::AreEqual(std::string("*"), stream.str(), L"You see this message if name is  not the same");
+		}
 	};
 }

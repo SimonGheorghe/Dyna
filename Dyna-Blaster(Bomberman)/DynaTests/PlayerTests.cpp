26a3fd8646620a146a5f045919f48d26b92b5965
Ionatan <binzaruionatan@yahoo.com>
2019-12-07 15:12:05 +0200

    Added test for place bomb.

--- Dyna-Blaster(Bomberman)/DynaTests/PlayerTests.cpp
+++ Dyna-Blaster(Bomberman)/DynaTests/PlayerTests.cpp
@@ -27,30 +27,40 @@ namespace DynaTests
 			uint16_t f = player.GetFire();
 			player.UpdatePlayerPower(Powers::Type::FireUp);
 			Assert::IsTrue(player.GetFire() == f + 1);
+
 			f = player.GetFire();
 			player.UpdatePlayerPower(Powers::Type::FireDown);
 			Assert::IsTrue(player.GetFire() == f - 1);
+
 			f = player.GetNoOfBombs();
 			player.UpdatePlayerPower(Powers::Type::BombUp);
 			Assert::IsTrue(player.GetNoOfBombs() == f + 1);
+
 			f = player.GetNoOfBombs();
 			player.UpdatePlayerPower(Powers::Type::BombDown);
 			Assert::IsTrue(player.GetNoOfBombs() == f - 1);
+
 			f = player.GetSpeed();
 			player.UpdatePlayerPower(Powers::Type::SkateUp);
 			Assert::IsTrue(player.GetSpeed() == f + 1);
+
 			f = player.GetSpeed();
 			player.UpdatePlayerPower(Powers::Type::SkateDown);
 			Assert::IsTrue(player.GetSpeed() == f - 1);
+
 			player.UpdatePlayerPower(Powers::Type::SoftBlockPass);
 			Assert::IsTrue(player.GetHasSoftBlockPass() == true);
+
 			player.UpdatePlayerPower(Powers::Type::BombPass);
 			Assert::IsTrue(player.GetHasBombPass() == true);
+
 			f = player.GetHealth();
 			player.UpdatePlayerPower(Powers::Type::Heart);
 			Assert::IsTrue(player.GetHealth() == f + 1);
+
 			player.UpdatePlayerPower(Powers::Type::Vest);
 			Assert::IsTrue(player.GetHasVest() == true);
+
 			player.UpdatePlayerPower(Powers::Type::RemoteControl);
 			Assert::IsTrue(player.GetHasRemoteControl() == true);
 		}
@@ -84,5 +94,20 @@ namespace DynaTests
 			Assert::IsTrue(player.GetCoordX() == 0 && player.GetCoordY == 1);
 		}
 
+		TEST_METHOD(PlaceBomb)
+		{
+			Player player(1, 1, 3, 0, 2);
+
+			Map map(Map::Stage::TheWall, 0);
+			map.SetWidth(Map::Width::Narrow);
+			map.SetLength(Map::Length::Short);
+			map.Create();
+
+			uint16_t x = 0, y = 0;
+			player.Place(map, x, y);
+			player.Move(map, 'd');
+			player.PlaceBomb(map);
+			Assert::IsTrue(player[0]->GetCoordX() == 0 && player[0]->GetCoordY == 1);
+		}
 	};
 }

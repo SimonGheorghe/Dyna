a5e7cd180a0f48aa56fb9be26536b143c9a124da
louiscomnoiu <louis.comnoiu@gmail.com>
2019-12-15 22:30:19 +0200

    Added  "ClearMap" test for Map class.

--- Dyna-Blaster(Bomberman)/DynaTests/MapTests.cpp
+++ Dyna-Blaster(Bomberman)/DynaTests/MapTests.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "CppUnitTest.h"
-#include "../Dyna-Blaster(Bomberman)/Map.h"
+
+#include "../Dyna-Blaster(Bomberman)/Player.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -9,6 +10,7 @@ namespace DynaTests
 	TEST_CLASS(MapTests)
 	{
 	public:
+
 		TEST_METHOD(Constructor)
 		{
 			Map map (Map::Stage::TheWall, 0);
@@ -20,6 +22,27 @@ namespace DynaTests
 			std::stringstream stream;
 			stream << map;
 			Assert::AreEqual(std::string("River --- Round 2"), stream.str(), L"You see this message if the blocks are not the same");
+		}
+
+		TEST_METHOD(ClearMap)
+		{
+			uint16_t playerFire = 5;
+			uint16_t playerNoOfBombs = 1;
+			uint16_t playerHealth = 5;
+			uint16_t playerScore = 0;
+			uint16_t playerSpeed = 2;
+			Player player(playerFire, playerNoOfBombs, playerHealth, playerScore, playerSpeed);
+
+			Map map(Map::Stage::TheWall, 0);
+			map.SetWidth(Map::Width::Narrow);
+			map.SetLength(Map::Length::Short);
+			map.Create();
+			player.Place(map, 1, 1);
+			player.PlaceBomb(map);
+			player.ExplodeBomb(map, 0);
+			map.ClearMap(1,1);
+
+			Assert::IsTrue(dynamic_cast<Block*>(map[{1, 1}])->GetType() == Block::Type::NoneBlock);
 		}
 	};
 }
